618886fc8501a3627371873e69bce9e15f70c91d
tay <taylyq@yahoo.com>
2015-08-14 00:22:13 -0500

    added bootstrap

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	#has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  	#validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+	has_attached_file :image, styles: { medium: "400x600#" }
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
